876f7c52e5d4fa89cdcb588aa5b044ab37f1b077
Marcelo Cunha <GitHubMarcelo@users.noreply.github.com>
2017-09-25 16:54:43 -0700

    Upload inicial
    Esse script cria uma VM com o Windows Server 2016 no Azure, a VM é criada a partir de uma imagem base customizada.

---
+++ CreateVMFromCustomImage.ps1
@@ -0,0 +1,13 @@
+﻿$nic1 = Get-AzureRmNetworkInterface -Name Nic1 -ResourceGroupName TestRG
+$cred = Get-Credential
+
+$VirtualMachine = New-AzureRmVMConfig -VMName "VM1" -VMSize "Standard_D2"
+
+$VirtualMachine = Set-AzureRmVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName "Server1" -Credential $cred
+
+$VirtualMachine = Set-AzureRmVMOSDisk -VM $VirtualMachine -Name "VM1-OSDisk.vhd" -VhdUri "https://mccmstorageaccount.blob.core.windows.net/vhds/VM1-OSDisk.vhd" -SourceImageUri "https://mccmstorageaccount.blob.core.windows.net/vhds/Base-W16.vhd" -CreateOption FromImage -Windows -Caching ReadWrite
+
+$VirtualMachine = Add-AzureRmVMNetworkInterface -VM $VirtualMachine -NetworkInterface $nic1
+
+New-AzureRmVM -VM $VirtualMachine -ResourceGroupName TestRG -Location eastus
+
